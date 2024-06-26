ecfbfa24e719c57ecb415edd122b6540aad3ed33
CarlStrbrg <carl.stromberg@opusonline.co>
2024-06-26 16:17:07 +0300

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import CustomButton from "@/components/CustomButton";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import SearchBar from "@/components/SearchBar";
+import CustomFilter from "@/components/CustomFilter";
+import SearchManufacturer from "@/components/SearchManufacturer";
+import CarCard from "@/components/CarCard";
+import CarDetails from "@/components/CarDetails";
+import ShowMore from "@/components/ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore,
+};
